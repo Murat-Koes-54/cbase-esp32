fab8a0de73b4d45ed56c6cf658f045a7d23cbd1e
Murat KÖS <68046660+Murat-Koes-54@users.noreply.github.com>
2023-07-22 23:25:50 +0200

    Add error code definitions

---
+++ src/cbase_esp32.h
@@ -0,0 +1,8 @@
+#if !( defined( EBX_CBASE_ESP32_H ) )
+#define EBX_CBASE_ESP32_H
+
+#include "inc/cbase_esp32_compiler.h"
+#include "inc/cbase_esp32_basetypes.h"
+#include "inc/cbase_esp32_err.h"
+
+#endif /* ifndef EBX_CBASE_ESP32_H */
